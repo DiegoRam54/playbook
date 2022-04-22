2fcdb15a108e73b8086211fcb1737716c66de92e
DiegoRam <diegoram54@outlook.com>
2022-04-21 22:26:26 -0500

    10. Modificando la UnitTest

--- weekly_mission_3/twitter/tests/models/User.test.js
+++ weekly_mission_3/twitter/tests/models/User.test.js
@@ -2,12 +2,12 @@ const User = require('./../../app/models/user');
 
 describe("Unit Test for User class", () => {
     test('Create an user oject', () => {
-        const user=new User(1, "DiegoRam", "Diego", "Bio", "dateCreated" ,"lastUpdated");
+        const user=new User(1, "DiegoRam", "Diego", "Bio");
         expect(user.id).toBe(1);
         expect(user.userName).toBe("DiegoRam");
         expect(user.name).toBe("Diego");
         expect(user.bio).toBe("Bio");
-        expect(user.dateCreated).toBe("dateCreated");
-        expect(user.lastUpdated).toBe("lastUpdated");
+        expect(user.dateCreated).not.toBeUndefined();
+        expect(user.lastUpdated).not.toBeUndefined();
     });
 });
