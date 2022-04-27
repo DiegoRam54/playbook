6f7f799b20bdf994529ed01853c6c2ddb0a6a05b
DiegoRam <diegoram54@outlook.com>
2022-04-26 21:16:47 -0500

    23. Modificando el cÃodigo haciendo pasar la UnitTest => 2.

--- weekly_mission_3/twitter/app/models/services/UserService.js
+++ weekly_mission_3/twitter/app/models/services/UserService.js
@@ -1,7 +1,15 @@
 const User = require('./../../models/User'); //importamos la clase que ya habíamos creado para crear el objeto
 class UserService{
     static create(id, userName, name){
-        return new User(1, "DiegoRam", "Diego", "Sin bio"); //creamos el objeto a partir del new, sin asignarlo a nada
+        return new User(id, userName, name, "Sin bio"); //creamos el objeto a partir del new, sin asignarlo a nada
+    }
+    static getInfo(user){
+        let array=new Array();
+        array[0]=user.id;
+        array[1]=user.userName;
+        array[2]=user.name;
+        array[3]=user.bio;
+        return array;
     }
 }
 module.exports = UserService;
