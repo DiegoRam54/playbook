fe63a45dd0a6f812ee3f46100a4e82e74b9d3e0f
DiegoRam <diegoram54@outlook.com>
2022-04-26 20:51:14 -0500

    21.1 Comentando el código

--- weekly_mission_3/twitter/app/models/services/UserService.js
+++ weekly_mission_3/twitter/app/models/services/UserService.js
@@ -1,7 +1,7 @@
-const User = require('./../../models/User');
+const User = require('./../../models/User'); //importamos la clase que ya habíamos creado para crear el objeto
 class UserService{
     static create(id, userName, name){
-        return new User(1, "DiegoRam", "Diego", "Sin bio");
+        return new User(1, "DiegoRam", "Diego", "Sin bio"); //creamos el objeto a partir del new, sin asignarlo a nada
     }
 }
 module.exports = UserService;
