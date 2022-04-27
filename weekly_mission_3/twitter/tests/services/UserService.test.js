7b2803c1cec02cd632dbc0c2bb9393189eb3e9b0
DiegoRam <diegoram54@outlook.com>
2022-04-26 22:02:14 -0500

    25. Agregando nueva UnitTest => 4.

--- weekly_mission_3/twitter/tests/services/UserService.test.js
+++ weekly_mission_3/twitter/tests/services/UserService.test.js
@@ -19,4 +19,13 @@ describe("Tests for UserService", () => {
         UserService.updateUserName(user, "DiegoR");
         expect(user.userName).toBe("DiegoR");
     });
+    test('4. Given a list of users give me the list of usernames', () => {
+        const user = UserService.create(1, "DiegoRam", "Diego");
+        const user2 = UserService.create(1, "DiegoRam1", "Diego");
+        const user3 = UserService.create(1, "DiegoRam2", "Diego");
+        const userNames = UserService.getAllUserNames([user, user2, user3]);
+        expect(userNames).toContain("DiegoRam");
+        expect(userNames).toContain("DiegoRam1");
+        expect(userNames).toContain("DiegoRam2");
+    });
 });
