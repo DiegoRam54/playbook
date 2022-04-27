d6f6e8f920ab33eb2991f40bc2053ef8ee32b760
DiegoRam <diegoram54@outlook.com>
2022-04-26 20:21:15 -0500

    18. Agregando UnitTest para UserService

---
+++ weekly_mission_3/twitter/tests/services/UserServices.test.js
@@ -0,0 +1,9 @@
+const UserService = require('./../../app/services/UserService');
+describe("Tests for UserService", () => {
+    test('1. Create a new user using de UserService', () => {
+        const user = UserService.create(1, "DiegoRam", "Diego");
+        expect(user.id).toBe(1);
+        expect(user.userName).toBe("DiegoRam");
+        expect(user.name).toBe("Diego");
+    });
+});
