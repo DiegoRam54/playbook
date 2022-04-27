9f46c8232271c993d9470eabadb6bf287cb692ce
DiegoRam <diegoram54@outlook.com>
2022-04-26 20:54:29 -0500

    22. Creando nueva prueba=> 2.

--- weekly_mission_3/twitter/tests/services/UserService.test.js
+++ weekly_mission_3/twitter/tests/services/UserService.test.js
@@ -6,4 +6,12 @@ describe("Tests for UserService", () => {
         expect(user.userName).toBe("DiegoRam");
         expect(user.name).toBe("Diego");
     });
+    test('2. Get all user data in a list', () => {
+        const user = UserService.create(1, "DiegoRam", "Diego");
+        const userInfoList = UserService.getInfo(user);
+        expect(userInfoList[0]).toBe(1);
+        expect(userInfoList[1]).toBe("DiegoRam");
+        expect(userInfoList[2]).toBe("Diego");
+        expect(userInfoList[3]).toBe("Sin bio");
+    });
 });
